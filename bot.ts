93d991a5c6e7023f1733c321418e6c2d090599b5
Eduardo Santizo <eddysanoli@gmail.com>
2022-10-29 10:38:13 -0600

    Re-ordered TS Files
    Moved the helper functions of the project to a src folder. The only script that was left in the root folder was "bot.ts". Also updated the import found in "bot.ts"

--- bot.ts
+++ bot.ts
@@ -1,7 +1,7 @@
 // Dependencies
 const DotEnv = require("dotenv").config({path: `${__dirname}/.env`});
 const cron = require("cron");
-const aws = require(__dirname + "/functions-aws");
+const aws = require(__dirname + "/src/functions-aws");
 
 // Discord client instance
 const { Client, Intents } = require('discord.js');
